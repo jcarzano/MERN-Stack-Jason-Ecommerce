402a700c7a4325c9464b8b08d9cc94baa088c994
Jason Carzano <json.carzano@gmail.com>
2026-08-27 10:05:44 +0800

    [Fix][Jason] Change text size

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -35,7 +35,7 @@ const FeaturedProducts = ({ featuredProducts }) => {
   return (
     <div className="py-12">
       <div className="container mx-auto px-4">
-        <h2 className="text-center text-4xl sm:text-5xl font-bold text-emerald-400 mb-4">
+        <h2 className="text-center text-3xl sm:text-4xl font-bold text-emerald-400 mb-4">
           Featured
         </h2>
         <div className="relative">
