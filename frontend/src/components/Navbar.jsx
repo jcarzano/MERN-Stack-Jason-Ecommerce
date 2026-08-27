b3df11946294a58ed932c5ac81adc11ad0b855cf
Jason Carzano <json.carzano@gmail.com>
2026-08-27 10:52:56 +0800

    [Modify][Jason] Modify Logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,13 +18,16 @@ const Navbar = () => {
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
       <div className="container mx-auto px-4 py-3">
         <div className="flex flex-wrap justify-between items-center">
-          <Link
-            to="/"
-            className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex"
-          >
-            Shappiii
+          <Link to="/" className="flex items-center">
+            <img
+              src="/logoNi.png"
+              alt="Logo"
+              className="h-10 w-auto object-contain"
+            />
+            <p className="text-2xl font-bold text-emerald-400 items-center space-x-2 flex">
+              Shappiii
+            </p>
           </Link>
-		  
 
           <nav className="flex flex-wrap items-center gap-4">
             <Link
